4c7763f9782fba4c491f46d5dbf27ea80789f5e4
dawko11 <dmiter90@gmail.com>
2024-06-29 22:21:05 +0200

    Final result

--- fake_data.py
+++ fake_data.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import numpy as np
+import os
+
+os.makedirs('data', exist_ok=True)
 
 data = {
     'UserID': range(1, 101),
     'Age': np.random.randint(18, 65, size=100),
     'Gender': np.random.choice(['Male', 'Female'], size=100),
-    'TimeSpent': np.random.randint(5, 300, size=100), 
+    'TimeSpent': np.random.randint(5, 300, size=100),
     'ContentType': np.random.choice(['News', 'Sports', 'Entertainment', 'Education'], size=100),
     'Device': np.random.choice(['Mobile', 'Desktop', 'Tablet'], size=100)
 }
@@ -14,4 +17,5 @@ df = pd.DataFrame(data)
 
 df.to_csv('data/fake_media_consumption.csv', index=False)
 
-print("Fikcyjne dane zostały wygenerowane i zapisane w pliku 'data/fake_media_consumption.csv'.")
+print("The data has been generated and saved to a file 'data/fake_media_consumption.csv'.")
+
